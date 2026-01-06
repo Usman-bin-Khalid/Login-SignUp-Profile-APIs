82a7fbb67a61d516817d957ecd9d46b65853e699
Usman-bin-Khalid <usmanbinkhalidpk@gmail.com>
2026-01-06 08:13:12 +0500

    Update and Delete Routes Correct

--- routes/socialRoutes.js
+++ routes/socialRoutes.js
@@ -6,10 +6,13 @@ const upload = require('../config/cloudinary');
 
 // All routes require login
 router.post('/post', authMiddleware, upload.single('image'), socialCtrl.createPost); // Create
+router.put('/post/:postId', authMiddleware, upload.single('image'), socialCtrl.updatePost); // Update
+router.delete('/post/:postId', authMiddleware, socialCtrl.deletePost); // Delete
 router.get('/all-posts', authMiddleware, socialCtrl.getAllPosts); // Discovery
 router.get('/search', authMiddleware, socialCtrl.searchUsers);
 router.get('/feed', authMiddleware, socialCtrl.getFeed);
 router.put('/like/:postId', authMiddleware, socialCtrl.toggleLike);
 router.post('/follow/:id', authMiddleware, socialCtrl.followUser);
+
 
 module.exports = router;  
