670fe81f30944492b7f45ed1243ec84897b7be89
Usman-bin-Khalid <usmanbinkhalidpk@gmail.com>
2026-01-09 07:23:25 +0500

    Order Model Completed

---
+++ models/Order.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    items: Array,
+    totalPrice: Number,
+    status: { type: String, enum: ['Pending', 'Shipped', 'Delivered'], default: 'Pending' }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Order', orderSchema);
