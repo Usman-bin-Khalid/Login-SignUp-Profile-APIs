29913795e158dac634730e0567e8db8fd009036c
Usman-bin-Khalid <usmanbinkhalidpk@gmail.com>
2026-01-12 22:22:44 +0500

    Role Middleware Completed

---
+++ middleware/roleMiddleware.js
@@ -0,0 +1,12 @@
+// middleware/roleMiddleware.js
+const authorize = (roles = []) => {
+    return (req, res, next) => {
+        // req.user is usually set by your JWT verify middleware
+        if (!roles.includes(req.user.role)) {
+            return res.status(403).json({ message: "Access Denied: Unauthorized Role" });
+        }
+        next();
+    };
+};
+
+module.exports = authorize;
