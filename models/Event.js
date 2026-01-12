5f530e28c35c2eaf76887db7b0f2a44bb0e756a5
Usman-bin-Khalid <usmanbinkhalidpk@gmail.com>
2026-01-12 22:21:16 +0500

    Event Model Completed

---
+++ models/Event.js
@@ -0,0 +1,13 @@
+// models/Event.js
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    date: { type: Date, required: true },
+    venue: { type: String, required: true },
+    organizer: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    capacity: { type: Number, required: true },
+    attendees: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Event', eventSchema);
